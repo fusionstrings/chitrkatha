11c6ffcee0a8ec1455d6704d289a29f3fd88d787
Dilip Kr. Shukla <contact@fusionstrings.com>
2021-02-28 01:34:22 +0000

    update xkcd offline html

--- src/xkcd-service-worker.js
+++ src/xkcd-service-worker.js
@@ -16,7 +16,7 @@ Copyright 2015, 2019 Google Inc. All Rights Reserved.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
-const OFFLINE_URL = 'offline.html';
+const OFFLINE_URL = '/xkcd/offline.html';
 const filesToCache = [
 	OFFLINE_URL,
 	'/apple-touch-icon.png',
