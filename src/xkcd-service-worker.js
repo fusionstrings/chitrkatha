de230a81e86e84417d867f6d75b2ec95369c4ca4
Dilip Kr. Shukla <contact@fusionstrings.com>
2021-03-07 22:04:26 +0000

    quick-fix for fetching latest comics

--- src/xkcd-service-worker.js
+++ src/xkcd-service-worker.js
@@ -56,7 +56,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
 	// We only want to call event.respondWith() if this is a navigation request
 	// for an HTML page.
-
+	const {pathname} = new URL(event.request.url)
 	if (event.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') {
 		return;
 	}
@@ -80,6 +80,30 @@ self.addEventListener('fetch', (event) => {
 
 				const cache = await caches.open(CACHE_NAME);
 				const cachedResponse = await cache.match(OFFLINE_URL);
+				return cachedResponse;
+			}
+		})());
+	} else if(pathname === '/api/comics/latest'){
+
+		event.respondWith((async () => {
+			try {
+				// First, try to use the navigation preload response if it's supported.
+				const preloadResponse = await event.preloadResponse;
+				if (preloadResponse) {
+					return preloadResponse;
+				}
+
+				const networkResponse = await fetch(event.request);
+				return networkResponse;
+			} catch (error) {
+				// catch is only triggered if an exception is thrown, which is likely
+				// due to a network error.
+				// If fetch() returns a valid HTTP response with a response code in
+				// the 4xx or 5xx range, the catch() will NOT be called.
+				console.log('Fetch failed; returning cached response instead.', error);
+
+				const cache = await caches.open(CACHE_NAME);
+				const cachedResponse = await cache.match('/api/comics/latest');
 				return cachedResponse;
 			}
 		})());
